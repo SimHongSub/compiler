37c1fc89b90cbd3e580f492bdff23475bf8dd3fe
SimHongsub <ghdtjq2038@gmail.com>
2020-05-26 23:15:02 +0900

    method separation

--- src/compiler/lexical/lexer/DFA.java
+++ src/compiler/lexical/lexer/DFA.java
@@ -3,8 +3,22 @@ package compiler.lexical.lexer;
 import java.util.*;
 
 import compiler.lexical.token.InputString;
+import compiler.lexical.token.Token;
 
+/** 
+ * Class to create DFA object according to states and transitions.
+ * 
+ * @date 2020.05.04
+ * @author SimHongSub
+ * @version 1.0
+ */
 public class DFA {
+	/**
+	 * type - The kind of tokens classified by DFA.
+	 * start - Start state of DFA.
+	 * ends - Final states of DFA.
+	 * transition - Transitions of each state.
+	 */
 	private String type;
 	private String start;
 	private Set<String> ends;
@@ -35,116 +49,150 @@ public class DFA {
 		
 		this.type = type;
 	}
-
-	public boolean match(InputString inputText) {
+	
+	/**
+	 * Returns whether or not the DFA accepts the string.
+	 * 
+	 * @param source - InputString object where input c source file content is stored
+	 * @param tokens - Token list to classify the integer and arithmetic
+	 * @return boolean
+	 * @exception NullPointerException
+	 */
+	public boolean match(InputString source, List<Token> tokens) {
 		String state = start;
 		
-		String s = inputText.getContentString();
+		String s = source.getContentString();
 		int tempPosition = -1;
 		
 		for (int i=0; i<s.length(); i++) {
 			char c = s.charAt(i);
 			if(this.type == "identifier") {
 				if (!transitions.get(state).containsKey(c)) {
-					if(ends.contains(state)) {
-						s = s.substring(0, i);
-						if(s.equals("int") || s.equals("char") || s.equals("bool") || s.equals("float") 
-								|| s.equals("if") || s.equals("else") || s.equals("while") || s.equals("for") || s.equals("return") 
-								|| s.equals("true") || s.equals("false")) {
-							
-							return false;
-						}else {
-							inputText.setNextPosition(i);
-							
-							return true;
-						}
-					}else {
-						if(i >= inputText.getErrorPosition()) {
-							inputText.setErrorPosition(i);
-							inputText.setErrorMessage("Token : " + inputText.getContentString().substring(0, i));
-						}
-						
-						return false;
-					}
-				}
-				
-				state = transitions.get(state).get(c);
-			}else if(this.type == "float") {
-				if (!transitions.get(state).containsKey(c)) {
-					if(tempPosition == -1) {
-						if(i+1 >= inputText.getErrorPosition()) {
-							inputText.setErrorPosition(i+1);
-							inputText.setErrorMessage("Token : " + inputText.getContentString().substring(0, i+1));
-						}
-						
-						return false;
-					}else {
-						inputText.setNextPosition(tempPosition);
-						
-						return true;
-					}
-				}
 					
+					return identifierMatch(state, s, i, c, source);
+				}
+				
 				state = transitions.get(state).get(c);
 				
-				if(ends.contains(state)) {
-					tempPosition = i+1;
-				}
-			}else if(this.type == "integer") {
+			}else if(this.type == "float" || this.type == "integer") {
 				try {
 					if (!transitions.get(state).containsKey(c)) {
-						if(tempPosition == -1) {
-							if(i+1 >= inputText.getErrorPosition()) {
-								inputText.setErrorPosition(i+1);
-								inputText.setErrorMessage("Token : " + inputText.getContentString().substring(0, i+1));
-							}
-							
+						if (tempPosition == -1) {
+
 							return false;
-						}else {
-							inputText.setNextPosition(tempPosition);
-							
-							return true;
+						} else {
+							if(s.substring(0, tempPosition).contains("-")) {
+								
+								return classifyArithmetic(tokens, tempPosition, source);
+							}else {
+								source.setNextPosition(tempPosition);
+
+								return true;
+							}
 						}
 					}
-					
+						
 					state = transitions.get(state).get(c);
 					
 					if(ends.contains(state)) {
 						tempPosition = i+1;
 					}
-				}catch (NullPointerException e) {
-					inputText.setNextPosition(tempPosition);
+				}catch(NullPointerException e) {
+					source.setNextPosition(tempPosition);
 					
 					return true;
 				}
+				
 			}else {
-				//try {
-					if (!transitions.get(state).containsKey(c)) {
-						if(i+1 >= inputText.getErrorPosition()) {
-							inputText.setErrorPosition(i+1);
-							inputText.setErrorMessage("Token : " + inputText.getContentString().substring(0, i+1));
-						}
-						
-						return false;
-					}
-					state = transitions.get(state).get(c);
-					
-					if(ends.contains(state)) {
-						
-						inputText.setNextPosition(i+1);
-						
-						return true;
-					}
-				/*}catch (NullPointerException e) {
+				if (!transitions.get(state).containsKey(c)) {
+					source.setErrorPosition(i+1);
+					source.setErrorMessage("Untokenized input c source code : \n" + source.getContentString() + "\nError Token : \" " + source.getContentString().substring(0, i+1) + " \"");
 					
 					return false;
-				}*/
+				}
+				state = transitions.get(state).get(c);
 				
+				if(ends.contains(state)) {
+					source.setNextPosition(i+1);
+						
+					return true;
+				}
 			}
 		}
 		
 		return false;
+	}
+	
+	/**
+	 * Returns whether or not the identifier DFA accepts the string.
+	 * 
+	 * @param state - Current state in DFA
+	 * @param s - Source content stored in the InputString object
+	 * @param position - The position of the character to check
+	 * @param positionChar - Character to check
+	 * @param source - InputString object where input c source file content is stored
+	 * @return boolean
+	 */
+	private boolean identifierMatch(String state, String s, int position, char positionChar, InputString source) {
 		
-		//return ends.contains(state);
+		if(ends.contains(state)) {
+			s = s.substring(0, position);
+			if(s.equals("int") || s.equals("char") || s.equals("bool") || s.equals("float") 
+					|| s.equals("if") || s.equals("else") || s.equals("while") || s.equals("for") || s.equals("return") 
+					|| s.equals("true") || s.equals("false")) {
+				
+				return false;
+			}else {
+				source.setNextPosition(position);
+				
+				return true;
+			}
+		}else {
+			
+			return false;
+		}
+	}
+	
+	/**
+	 * Returns whether or not the Arithmetic.
+	 * 
+	 * @param tokens - Tokenized token list
+	 * @param tempPosition - Current token position
+	 * @param source - InputString object where input c source file content is stored
+	 * @return boolean
+	 */
+	private boolean classifyArithmetic(List<Token> tokens, int tempPosition, InputString source) {
+		Token token = tokens.get(tokens.size() - 1);
+		
+		if(token.getTokenName().equals("INT")||token.getTokenName().equals("FLOAT")||token.getTokenName().equals("ID")) {
+			
+			return false;
+		}else {
+			if(token.getTokenName().equals("RPAREN")) {
+				int countLPAREN = 0;
+				int countRPAREN = 0;
+				
+				for(Token temp : tokens) {
+					if(temp.getTokenName().equals("LPAREN")) {
+						countLPAREN++;
+					}else if(temp.getTokenName().equals("RPAREN")) {
+						countRPAREN++;
+					}
+				}
+				
+				if(countLPAREN == countRPAREN) {
+					
+					return false;
+				}else {
+					source.setNextPosition(tempPosition);
+					
+					return true;
+				}
+			}else {
+				source.setNextPosition(tempPosition);
+
+				return true;
+			}
+		}
 	}
 }
