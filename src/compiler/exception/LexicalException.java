9f84a39aab6090e8818c42fde98333a6ea79d565
SimHongsub <ghdtjq2038@gmail.com>
2020-05-26 23:17:02 +0900

    Add comment.

--- src/compiler/exception/LexicalException.java
+++ src/compiler/exception/LexicalException.java
@@ -1,7 +1,18 @@
 package compiler.exception;
 
+/** 
+ * Class responsible for lexical analyzer exception.
+ * 
+ * @date 2020.05.05
+ * @author SimHongSub
+ * @version 1.0
+ */
+@SuppressWarnings("serial")
 public class LexicalException extends Exception {
-	
+	/**
+	 * errorPosition - Variable that stores the location of error to be displayed in the console.
+	 * message - String variable to stores the error message to be displayed in the console.
+	 */
 	private int errorPosition;
 	private String message;
 	
@@ -10,12 +21,22 @@ public class LexicalException extends Exception {
 		this.message = message;
 	}
 	
+	/**
+	 * The get method to returns error token position.
+	 * 
+	 * @return tokenName
+	 */
 	public int getErrorPosition() {
 		return errorPosition;
 	}
 	
+	/**
+	 * The get method to returns error message.
+	 * 
+	 * @return tokenName
+	 */
 	public String getMessage() {
-		return message;
+		return "\n" + message;
 	}
 
 }
--- src/compiler/lexical/token/InputString.java
+++ src/compiler/lexical/token/InputString.java
@@ -1,6 +1,19 @@
 package compiler.lexical.token;
 
+/** 
+ * Class to create input string(c source code) object.
+ * 
+ * @date 2020.05.05
+ * @author SimHongSub
+ * @version 1.0
+ */
 public class InputString {
+	/**
+	 * nextPosition - Variable to save the token position used to cut the token in the entire content.
+	 * contentString - String variable to save the entire content(c source code).
+	 * errorPosition - Variable to save the error token position not classified in any DFA.
+	 * errorMessage - String variable to save the error message.
+	 */
 	private int nextPosition;
 	private String contentString;
 	private int errorPosition;
@@ -12,6 +25,12 @@ public class InputString {
 		this.errorPosition = 0;
 	}
 	
+	/**
+	 * The get, set method to returns next character position and set nextPosition value.
+	 * 
+	 * @param nextPosition
+	 * @return tokenName
+	 */
 	public int getNextPosition() {
 		return nextPosition;
 	}
@@ -19,7 +38,13 @@ public class InputString {
 	public void setNextPosition(int nextPosition) {
 		this.nextPosition = nextPosition;
 	}
-
+	
+	/**
+	 * The get, set method to returns source content and set source content value.
+	 * 
+	 * @param contentString
+	 * @return tokenName
+	 */
 	public String getContentString() {
 		return contentString;
 	}
@@ -27,15 +52,26 @@ public class InputString {
 	public void setContentString(String contentString) {
 		this.contentString = contentString;
 	}
-
+	
+	/**
+	 * The get, set method to returns error token position and set position value.
+	 * 
+	 * @param errorPosition
+	 * @return tokenName
+	 */
 	public int getErrorPosition() {
 		return errorPosition;
 	}
-
+	
 	public void setErrorPosition(int errorPosition) {
 		this.errorPosition = errorPosition;
 	}
-
+	
+	/**
+	 * The get, set method to returns error message and set message value.
+	 * 
+	 * @return tokenName
+	 */
 	public String getErrorMessage() {
 		return errorMessage;
 	}
