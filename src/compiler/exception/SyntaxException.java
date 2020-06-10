5c2bf0f3f35610cd5a8631dd7504201d5c82e4d8
SimHongsub <ghdtjq2038@gmail.com>
2020-06-10 17:48:35 +0900

    add source to syntaxer exception handling

--- src/compiler/exception/SyntaxException.java
+++ src/compiler/exception/SyntaxException.java
@@ -1,5 +1,41 @@
 package compiler.exception;
 
-public class SyntaxException {
+/** 
+ * Class responsible for syntax analyzer exception.
+ * 
+ * @date 2020.06.10
+ * @author SimHongSub
+ * @version 1.0
+ */
+@SuppressWarnings("serial")
+public class SyntaxException extends Exception {
+	/**
+	 * message - String variable to stores the error message to be displayed in the console.
+	 */
+	private String message;
 
+	public SyntaxException(String state, String input, String syntaxString, int index) {
+		String inputCase;
+		
+		if(Character.isUpperCase(input.charAt(0))) {
+			inputCase = "GOTO";
+		}else {
+			inputCase = "ACTION";
+		}
+		
+		String temp = syntaxString.substring(0, index);
+		
+		syntaxString = temp + "| " + syntaxString.substring(index, syntaxString.length());
+		
+		message = "\nBar progress : " + syntaxString + "\nError state : " + state + "\nCase : " + inputCase + "\ninput : " + input;
+	}
+	
+	/**
+	 * The get method to returns error message.
+	 * 
+	 * @return message
+	 */
+	public String getMessage() {
+		return message;
+	}
 }
